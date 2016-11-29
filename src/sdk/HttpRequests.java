5e9c1b23e2ea9cceae663bca462210313c51928f
ChrisPals <christavio92@gmail.com>
2016-11-29 14:05:19 +0100

    printBook og printBooks virker

---
+++ src/sdk/HttpRequests.java
@@ -0,0 +1,49 @@
+package sdk;
+
+import com.sun.jersey.api.client.*;
+
+/**
+ * Created by Christofferpalsgaard on 24/11/2016.
+ */
+public class HttpRequests {
+
+    static Client client = Client.create();
+
+    public static ClientResponse post(String token, String path, String json) {
+        ClientResponse clientResponse = null;
+
+        try {
+            WebResource webResource = client
+                    .resource("http://localhost:8080/server2_0_war_exploded")
+                    .path(path);
+
+            clientResponse = webResource.accept("application/json").post(ClientResponse.class, json);
+        }
+        catch
+                (UniformInterfaceException | ClientHandlerException e) {
+            e.printStackTrace();
+        }
+        return clientResponse;
+    }
+    public static ClientResponse get(String path) {
+        ClientResponse clientResponse = null;
+
+        try {
+            WebResource webResource = client
+                    .resource("http://localhost:8080/server2_0_war_exploded")
+                    .path(path);
+
+
+            clientResponse = webResource.accept("application/json").get(ClientResponse.class);
+
+
+
+
+        } catch (UniformInterfaceException | ClientHandlerException e) {
+            e.printStackTrace();
+        }
+        return clientResponse;
+    }
+
+
+}
